92672afd5e37915effd1cb951cb9ed9259a6bb01
Leerlandais <leerlandais@gmail.com>
2023-12-20 18:59:49 +0100

    and it's done

--- scripts/easytiles.js
+++ scripts/easytiles.js
@@ -3,10 +3,10 @@ startTile.onclick = shuffleTiles;
 
 function shuffleTiles() {
     startTile.disabled = true;
-    for (var row = 1; row <= 4; row++) { 
-        for (var column = 1; column <= 4; column++) { 
-            var row2 = Math.floor(Math.random() * 4 + 1); 
-            var column2 = Math.floor(Math.random() * 4 + 1);    
+    for (var row = 1; row <= 3; row++) { 
+        for (var column = 1; column <= 3; column++) { 
+            var row2 = Math.floor(Math.random() * 3 + 1); 
+            var column2 = Math.floor(Math.random() * 3 + 1);    
             switchTilePos("tile" + row + column, "tile" + row2 + column2); 
         } 
     } 
@@ -16,28 +16,28 @@ function tileSelect(row, column) {
     console.log("clicked");
     var tile = document.getElementById("tile" + row + column) ; 
     var tile = tile.className;
-    if (tile!="tile16") { 
-        if (column < 4) {
-            if ( document.getElementById("tile" + row + (column + 1)).className=="tile16") {
+    if (tile!="tile9") { 
+        if (column < 3) {
+            if ( document.getElementById("tile" + row + (column + 1)).className=="tile9") {
                 switchTilePos("tile"+row+column,"tile" + row + (column + 1));
                 return;
             }
         }
         if (column > 1) {
-            if ( document.getElementById("tile" + row + (column - 1)).className=="tile16") {
+            if ( document.getElementById("tile" + row + (column - 1)).className=="tile9") {
                 switchTilePos("tile"+row+column,"tile" + row + (column - 1));
                 return;
             }
         }
         
         if (row > 1) {
-            if ( document.getElementById("tile" + (row - 1) + column).className=="tile16") {
+            if ( document.getElementById("tile" + (row - 1) + column).className=="tile9") {
                 switchTilePos("tile"+row+column,"tile" + (row - 1) + column);
                 return;
             }
         }
-        if (row < 4) {
-            if ( document.getElementById("tile" + (row + 1) + column).className=="tile16") {
+        if (row < 3) {
+            if ( document.getElementById("tile" + (row + 1) + column).className=="tile9") {
                 switchTilePos("tile"+row+column,"tile" + (row + 1) + column);
                 return;
             }
